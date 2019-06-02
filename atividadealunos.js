8f54fbcad3c0d167223e8f863a12f10a1fd6c75a
rebeca <rebecamsramos@gmail.com>
2019-06-01 21:54:07 -0400

    enviando atividade alunos

---
+++ atividadealunos.js
@@ -0,0 +1,7 @@
+var alunos = [ " Matheus " , " João Vitor " , " Gabriel " , " André " , " Raynara " , " Rebeca " , " Edgard " , " Israel " , " Naedson " , " Wanderson " ]
+
+var num_aleatorio = Math . random () * alunos . comprimento
+
+var sorteado = Matemática . round (num_aleatorio)
+
+console.log ( " Sorteado: " + alunos [sorteado - 1 ])
