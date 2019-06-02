dec40f05e2def1cc21fcd6dbbb4f6d9cfdc3faf5
309mari <rebecamsramos@gmail,com>
2019-06-02 18:14:55 -0300

    atividade aluno corrigida

--- atividadealunos.js
+++ atividadealunos.js
@@ -1,7 +1,7 @@
-var alunos = [ " Matheus " , " João Vitor " , " Gabriel " , " André " , " Raynara " , " Rebeca " , " Edgard " , " Israel " , " Naedson " , " Wanderson " ]
+var alunos = ["Matheus", "João Vitor", "Gabriel", "André", "Raynara", "Rebecca", "Edgard", "Israel", "Naedson", "Wanderson"]
 
-var num_aleatorio = Math . random () * alunos . comprimento
+var num_aleatorio = Math . random() * alunos.length
 
-var sorteado = Matemática . round (num_aleatorio)
+var sorteado = Math.round(num_aleatorio)
 
-console.log ( " Sorteado: " + alunos [sorteado - 1 ])
+console.log("Sorteado:" + alunos[sorteado-1])
