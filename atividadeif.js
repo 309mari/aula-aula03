cfd5e6fc476a40296b61099c8aa107d4d6afdada
rebeca <rebecamsramos@gmail.com>
2019-06-01 21:15:21 -0400

    enviando atividadeif

---
+++ atividadeif.js
@@ -0,0 +1,17 @@
+var idade = 17
+
+if(idade <= 0 && > 12) {
+  console.log("criança")
+
+} if else(idade <= 12 && > 19) {
+   console.log("adolescente")
+
+} if else(idade <= 19 && > 41) {
+   console.log("adulto")
+
+} if else(idade < 41) {
+   console.log("velho")
+
+}
+
+
