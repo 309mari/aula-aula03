a8c6d770062103505517597da8f7daf6a0fb8110
309mari <rebecamsramos@gmail,com>
2019-06-09 18:05:27 -0300

    functiob

---
+++ myfunctions.js
@@ -0,0 +1,12 @@
+function soma (5,15){
+    return 5 + 15
+}
+function subtracao (10,5){
+    return 10 - 5
+}
+function multiplicacao (5,5){
+    return 5 * 5
+}
+function divisao (20,2){
+    return 20 / 2
+}
